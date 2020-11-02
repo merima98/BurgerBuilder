b948008bd724e3adef153bdea24b87180aa63b0f
merima98 <merimaceranic98@gmail.com>
2020-11-02 14:27:09 +0100

    Using a logo in application

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,9 +1,10 @@
 import React from "react";
 import calsses from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
 const toolbar = (props) => (
   <header className={calsses.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>...</nav>
   </header>
 );

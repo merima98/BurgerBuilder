abb3caab9afbb4dfa67a0502a6524d7fd37d9101
merima98 <merimaceranic98@gmail.com>
2020-11-09 18:22:53 +0100

    Order submission and passing data between pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,28 +88,7 @@ class BurgerBuilder extends Component {
   };
   purchaseContinueHandler = () => {
     // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: "Merima Ceranic",
-    //     address: {
-    //       street: "TestStreet 1",
-    //       zipCode: "1230",
-    //       country: "Bosnia and Herzegovina",
-    //     },
-    //     email: "test@test.com",
-    //   },
-    //   deliveryMethod: "fastest",
-    // };
-    // axios
-    //   .post("/orders.json", order)
-    //   .then((response) => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   })
-    //   .catch((error) => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   });
+
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(
@@ -118,6 +97,7 @@ class BurgerBuilder extends Component {
           encodeURIComponent(this.state.ingredients[i])
       );
     }
+    queryParams.push("price=" + this.state.totalPrice);
     const queryString = queryParams.join("&");
     this.props.history.push({
       pathname: "checkout",

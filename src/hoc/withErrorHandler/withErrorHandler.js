faadcf820840ffeb22314c634236f733dc0bc385
merima98 <merimaceranic98@gmail.com>
2020-11-14 16:29:45 +0100

    Adjusting checkout and contact data

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -26,18 +26,6 @@ const withErrorHandler = (WrappedComponent, axios) => {
       axios.interceptors.response.eject(this.resInterceptor);
     }
 
-    // componentDidMount() {
-    //   axios.interceptors.request.use((req) => {
-    //     this.setState({ error: null });
-    //     return req;
-    //   });
-    //   axios.interceptors.response.use(
-    //     (res) => res,
-    //     (error) => {
-    //       this.setState({ error: error });
-    //     }
-    //   );
-    // }
     errorConfirmedHandler = () => {
       this.setState({ error: null });
     };
